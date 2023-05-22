43fd39ae3f49afa5f9efc3b86d540b570d59c811
SorayaYF <sorayayasin@hotmail.com>
2023-05-22 11:25:01 -0300

    Primeiro Commit

---
+++ src/main/java/primeira_aula/Pessoa.java
@@ -0,0 +1,51 @@
+package primeira_aula;
+
+public class Pessoa {
+
+	String nome, sexo;
+	double peso, altura;
+	
+	public double calculaIMC() {
+		double imc = peso/Math.pow(altura, 2);
+		return imc;
+	}
+	
+	public String distincaoIMC(double imc) {
+		String distincao ="";
+		
+		if(sexo.equals("F")) {
+			if(imc<19.1) {
+				distincao = "Abaixo do Peso";
+			}else if(imc>=19.1 && imc<25.9) {
+				distincao = "Peso Ideal";
+			}else if(imc>=25.9 && imc<27.4) {
+				distincao = "Pouco Acima do Peso";
+			}else if(imc>=27.4 && imc<32.4) {
+				distincao = "Acima do Peso";
+			} else {
+				distincao = "Obesidade";
+			}
+			
+		} else {	
+			if(imc<20.7) {
+				distincao = "Abaixo do Peso";
+			}else if(imc>=20.7 && imc<26.5) {
+				distincao = "Peso Ideal";
+			}else if(imc>=26.5 && imc<27.9) {
+				distincao = "Pouco Acima do Peso";
+			}else if(imc>=27.9 && imc<31.2) {
+				distincao = "Acima do Peso";
+			} else {
+				distincao = "Obesidade";
+			}
+		}
+		
+		return distincao;
+	}
+	
+	public String toString(double imc, String distincao) {
+		String mensagem = "Nome: "+nome+"\tSexo: "+sexo+"\t\tPeso: "+peso+"\tAltura: "+altura+
+						"\nIMC: "+String.format("%.2f", imc)+"\tDistincao: "+distincao+"\n";
+		return mensagem;
+	}
+}
---
+++ src/main/java/primeira_aula/Principal.java
@@ -0,0 +1,29 @@
+package primeira_aula;
+
+public class Principal {
+
+	public static void main(String[] args) {
+		
+		Pessoa jessica = new Pessoa();
+		jessica.nome = "Jessica";
+		jessica.sexo = "F";
+		jessica.peso = 56.0;
+		jessica.altura = 1.55;
+		double imc = jessica.calculaIMC();
+		String distincao = jessica.distincaoIMC(imc);
+		
+		System.out.println(jessica.toString(imc, distincao));
+		
+		Pessoa gabriel = new Pessoa();
+		gabriel.nome = "Gabriel";
+		gabriel.sexo = "M";
+		gabriel.peso = 80.0;
+		gabriel.altura = 1.68;
+		imc = gabriel.calculaIMC();
+		distincao= gabriel.distincaoIMC(imc);
+		
+		System.out.println(gabriel.toString(imc, distincao));
+
+	}
+
+}
