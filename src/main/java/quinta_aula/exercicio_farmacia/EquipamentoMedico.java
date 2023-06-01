0e79c6b1225dc0a96fb469be7c07c72a58eb599b
Taís Marcolino <tais.c.marcolino@gmail.com>
2023-06-01 11:00:37 -0300

    Implementação de frufru

--- src/main/java/quinta_aula/exercicio_farmacia/EquipamentoMedico.java
+++ src/main/java/quinta_aula/exercicio_farmacia/EquipamentoMedico.java
@@ -15,16 +15,15 @@ public class EquipamentoMedico extends Produto{
 	public boolean venda(Cliente cliente, int quantidade) {
 		if(temEstoque(quantidade)) {
 			return super.venda(cliente, quantidade);
-		} else {
-			do {
-				estoque = getEstoque()-1;
-				quantidade -=1;
-			}while(getEstoque()>0);
-			
-			qtdEncomendas += quantidade;
-			cliente.setSaldoDevedor(cliente.getSaldoDevedor()+(getValor()*quantidade));
-		}
-		
+		} 
+
+		do {
+			estoque = getEstoque() - 1;
+			quantidade -= 1;
+		} while (getEstoque() > 0);
+
+		qtdEncomendas += quantidade;
+		cliente.setSaldoDevedor(cliente.getSaldoDevedor() + (getValor() * quantidade));
 		return true;
 	}
 	
